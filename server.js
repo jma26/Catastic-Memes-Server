db9e1313b48c2e07d95e5d0291ee3bccd66071c3
Jesse Ma <jma26code@gmail.com>
2025-07-25 11:06:07 -0700

    Add express-rate-limiter dependency for basic rate limiting on /api route

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
+import { rateLimit } from 'express-rate-limit'
 import catRoutes from './routes/catRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  limit: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running smoothly!' });
 });
 
-app.use('/api', catRoutes);
+app.use('/api', limiter, catRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
